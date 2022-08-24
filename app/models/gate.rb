3ae793c45e9977cde79ff6446585cc410aa22a80
yura-poj <yurap6002@gmail.com>
2022-08-24 22:17:04 +0700

    add validates of user verify

--- app/models/gate.rb
+++ app/models/gate.rb
@@ -5,8 +5,17 @@ class Gate < ApplicationRecord
 
   validates :user_id, presence: true
   validates_inclusion_of :enter, in: [true, false]
+  validate :user_cannot_be_unverified
 
   def enter?
     enter
+  end
+
+  private
+
+  def user_cannot_be_unverified
+    unless User.find(user_id).verify
+      errors.add(:user_verification, "user must be verified")
+    end
   end
 end
