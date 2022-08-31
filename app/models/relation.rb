eb65ae57055ab53b3d2f7d530d8b2f2b27297bc8
yura-poj <yurap6002@gmail.com>
2022-08-31 22:05:34 +0700

    add validate uniquess

--- app/models/relation.rb
+++ app/models/relation.rb
@@ -4,4 +4,14 @@ class Relation < ApplicationRecord
   belongs_to :sender, class_name: 'User'
   belongs_to :addressee, class_name: 'User'
 
+  validates :sender_id, uniqueness: { scope: :addressee_id,
+                                 message: "Already have a relation" }
+
+  validate :custom_uniqueness
+
+  private
+
+  def custom_uniqueness
+    errors.add(:must, "Already have a relation") if Relation.find_by(sender_id: addressee_id, addressee_id: sender_id).present?
+  end
 end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -36,6 +36,8 @@ Dir[Rails.root.join('spec', 'support', '**', '*.rb')].each { |f| require f }
 RSpec.configure do |config|
   config.include FactoryBot::Syntax::Methods
   config.include ApiHelpers, type: :request
+  config.include Devise::Test::ControllerHelpers, type: :controller
+  config.include ControllerHelpers, type: :controller
 
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
