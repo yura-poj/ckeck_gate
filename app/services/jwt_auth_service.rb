1ae9f3b19c849c99f904abb7de516cbecb0b5767
yura-poj <yurap6002@gmail.com>
2022-11-16 21:37:17 +0700

    fix by rubocop

--- app/services/jwt_auth_service.rb
+++ app/services/jwt_auth_service.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class JwtAuthService
   HMAC_SECRET = Rails.application.credentials[Rails.env.to_sym][:jwt][:secret]
 
@@ -7,11 +9,10 @@ class JwtAuthService
 
   def encode
     payload = { data: 'work' }
-    token = JWT.encode payload, HMAC_SECRET, 'HS256'
+    JWT.encode payload, HMAC_SECRET, 'HS256'
   end
 
   def decode
-    decoded_token = JWT.decode @token, HMAC_SECRET, true, { algorithm: 'HS256' }
+    JWT.decode @token, HMAC_SECRET, true, { algorithm: 'HS256' }
   end
 end
-
